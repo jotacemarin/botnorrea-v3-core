f119854a7c92e2b309c031027ca746f71c62f6e0
jotacemarin <jotacemarin@gmail.com>
2025-11-10 21:05:57 -0500

    Add some fixes to debug command

--- src/functions/telegram-webhook/telegram-debug/index.ts
+++ src/functions/telegram-webhook/telegram-debug/index.ts
@@ -5,6 +5,8 @@ import {
 import { TelegramService } from "../../../services";
 import { getCommand } from "./get-command";
 
+const BOT_USERNAME = process.env.BOT_USERNAME ?? "";
+
 const getMessageToDebug = (body: Update_Telegram): string => {
   if (body?.message?.reply_to_message) {
     return JSON.stringify(body.message.reply_to_message, null, 2);
@@ -19,17 +21,17 @@ export const telegramDebug = async (body: Update_Telegram) => {
   }
 
   const key = getCommand(body);
-  if (key !== "/debug") {
-    return;
+  if (key === "/debug" || key === `/debug@${BOT_USERNAME}`) {
+    console.log(`telegramDebug: ${JSON.stringify(body)}`);
+    const messageToDebug = getMessageToDebug(body);
+
+    await TelegramService.sendMessage({
+      chat_id: body.message.chat.id,
+      reply_to_message_id: body.message.message_id,
+      parse_mode: FormattingOptions_Telegram.HTML,
+      text: `<code>${messageToDebug}</code>`,
+    });
   }
 
-  console.log(`telegramDebug: ${JSON.stringify(body)}`);
-  const messageToDebug = getMessageToDebug(body);
-
-  await TelegramService.sendMessage({
-    chat_id: body.message.chat.id,
-    reply_to_message_id: body.message.message_id,
-    parse_mode: FormattingOptions_Telegram.HTML,
-    text: `<code>${messageToDebug}</code>`,
-  });
+  return;
 };
